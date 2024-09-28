1b2fd177bd31ec104af9bee934ce80dd8a4c2d4d
polarboy <chinware@163.com>
2024-09-28 19:37:53 +0800

    Add ToolTip ShowCase

--- src/AtomUIGallery/ShowCases/ViewModels/TooltipShowCaseViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/TooltipShowCaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ReactiveUI;
+﻿using AtomUI.Controls;
+using Avalonia.Controls;
+using ReactiveUI;
 
 namespace AtomUIGallery.ShowCases.ViewModels;
 
@@ -13,5 +15,43 @@ public class TooltipShowCaseViewModel : ReactiveObject, IRoutableViewModel
     public TooltipShowCaseViewModel(IScreen screen)
     {
         HostScreen = screen;
+    }
+    
+    private bool _showArrow = true;
+
+    public bool ShowArrow
+    {
+        get => _showArrow;
+        set => this.RaiseAndSetIfChanged(ref _showArrow, value);
+    }
+
+    private bool _isPointAtCenter;
+
+    public bool IsPointAtCenter
+    {
+        get => _isPointAtCenter;
+        set => this.RaiseAndSetIfChanged(ref _isPointAtCenter, value);
+    }
+    
+    public void HandleSelectionChanged(object? sender, SelectionChangedEventArgs args)
+    {
+        if (sender is Segmented segmented)
+        {
+            if (segmented.SelectedIndex == 0)
+            {
+                ShowArrow       = true;
+                IsPointAtCenter = false;
+            }
+            else if (segmented.SelectedIndex == 1)
+            {
+                ShowArrow       = false;
+                IsPointAtCenter = false;
+            }
+            else if (segmented.SelectedIndex == 2)
+            {
+                IsPointAtCenter = true;
+                ShowArrow       = true;
+            }
+        }
     }
 }
--- src/AtomUIGallery/ShowCases/ViewModels/TreeViewShowCaseViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/TreeViewShowCaseViewModel.cs
@@ -9,6 +9,30 @@ public class TreeViewShowCaseViewModel : ReactiveObject, IRoutableViewModel
     public IScreen HostScreen { get; }
     
     public string UrlPathSegment { get; } = ID;
+    
+    private bool _showLineSwitchChecked = true;
+
+    public bool ShowLineSwitchChecked
+    {
+        get => _showLineSwitchChecked;
+        set => this.RaiseAndSetIfChanged(ref _showLineSwitchChecked, value);
+    }
+    
+    private bool _showIconSwitchChecked;
+
+    public bool ShowIconSwitchChecked
+    {
+        get => _showIconSwitchChecked;
+        set => this.RaiseAndSetIfChanged(ref _showIconSwitchChecked, value);
+    }
+
+    private bool _showLeafSwitcherSwitchChecked;
+
+    public bool ShowLeafSwitcherSwitchChecked
+    {
+        get => _showLeafSwitcherSwitchChecked;
+        set => this.RaiseAndSetIfChanged(ref _showLeafSwitcherSwitchChecked, value);
+    }
 
     public TreeViewShowCaseViewModel(IScreen screen)
     {
