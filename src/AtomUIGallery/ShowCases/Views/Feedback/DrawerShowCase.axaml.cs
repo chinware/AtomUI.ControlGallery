b00cbec642ed2203ab4d51564b636604e903bb0f
chinboy <chinboy@qinware.com>
2025-02-21 19:33:54 +0800

    Add Refactor Drawer

--- src/AtomUIGallery/ShowCases/Views/Feedback/DrawerShowCase.axaml.cs
+++ src/AtomUIGallery/ShowCases/Views/Feedback/DrawerShowCase.axaml.cs
@@ -1,6 +1,8 @@
-﻿using AtomUI.Controls;
+﻿using AtomUI;
+using AtomUI.Controls;
 using AtomUIGallery.ShowCases.ViewModels;
 using Avalonia;
+using Avalonia.Data.Converters;
 using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
@@ -9,10 +11,43 @@ namespace AtomUIGallery.ShowCases.Views;
 
 public partial class DrawerShowCase : ReactiveUserControl<DrawerViewModel>
 {
+    public static readonly IValueConverter PlacementTextConverter =
+        new FuncValueConverter<object?, object?>(x =>
+        {
+            if (x is int intValue)
+            {
+                var placement = (DrawerPlacement)intValue;
+                return placement.ToString();
+            }
+
+            return x;
+        });
     public DrawerShowCase()
     {
-        this.WhenActivated(disposables => { });
+        this.WhenActivated(disposables =>
+        {
+            if (DataContext is DrawerViewModel viewModel)
+            {
+            }
+        });
         InitializeComponent();
+    }
+
+    private void HandleOpenLargeSizeDrawer(object sender, RoutedEventArgs e)
+    {
+        // PresetSizeDrawer.SizeType = SizeType.Large;
+        // PresetSizeDrawer.IsOpen = true;
+    }
+    
+    private void HandleOpenDefaultSizeDrawer(object sender, RoutedEventArgs e)
+    {
+        // PresetSizeDrawer.SizeType = SizeType.Small;
+        // PresetSizeDrawer.IsOpen   = true;
+    }
+    
+    private void HandleOpenMultilevelLevelTwoDrawer(object sender, RoutedEventArgs e)
+    {
+        MultiLevelDrawerLevelTwo.IsOpen = true;
     }
     
     private void HandleButtonClick(object? sender, RoutedEventArgs e)
@@ -32,6 +67,6 @@ public partial class DrawerShowCase : ReactiveUserControl<DrawerViewModel>
 
     private void HandleButtonOpenOnCurrentParentOnClick(object? sender, RoutedEventArgs e)
     {
-        Drawer1.OpenOn = Drawer1.OpenOn?.Parent as Visual;
+        // Drawer1.OpenOn = Drawer1.OpenOn?.Parent as Visual;
     }
 }
