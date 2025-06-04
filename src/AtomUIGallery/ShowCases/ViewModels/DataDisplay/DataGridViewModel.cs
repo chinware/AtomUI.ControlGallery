50ce2f8caf7a724f6826bd2e9e878af164190056
CHINBOY <qinware@163.com>
2025-06-04 13:27:55 +0800

    Optimize DataGridLength

--- src/AtomUIGallery/ShowCases/ViewModels/DataDisplay/DataGridViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/DataDisplay/DataGridViewModel.cs
@@ -13,33 +13,47 @@ public partial class DataGridViewModel : ReactiveObject, IRoutableViewModel
     public string UrlPathSegment { get; } = ID;
     
     public ObservableCollection<DataGridBaseInfo> BasicCaseDataSource { get; }
+    public ObservableCollection<DataGridBaseInfo> FilterAndSorterDataSource { get; }
 
     public DataGridViewModel(IScreen screen)
     {
-        HostScreen = screen;
-        BasicCaseDataSource = new ObservableCollection<DataGridBaseInfo>();
+        HostScreen                = screen;
+        BasicCaseDataSource       = new ObservableCollection<DataGridBaseInfo>();
+        FilterAndSorterDataSource = new ObservableCollection<DataGridBaseInfo>();
         InitBasicShowCaseDataSource();
+        InitFilterAndSorterDataSource();
     }
 
     private void InitBasicShowCaseDataSource()
     {
         List<DataGridBaseInfo> items = [
-            new DataGridBaseInfo("1", "John Brown", 32, "New York No. 1 Lake Park", 
-                [
-                    new TagInfo() { Name = "NICE", Color = "green"},
-                    new TagInfo() { Name = "DEVELOPER", Color = "geekblue"}
-                ]),
-            new DataGridBaseInfo("2", "Jim Green", 42, "London No. 1 Lake Park", 
-                [
-                    new TagInfo() { Name = "LOSER", Color = "volcano"}
-                ]),
-            new DataGridBaseInfo("3", "Joe Black", 32, "Sydney No. 1 Lake Park", 
-                [
-                    new TagInfo() { Name = "COOL", Color      = "green"},
-                    new TagInfo() { Name = "TEACHER", Color = "geekblue"}
-                ])
+            new ("1", "John Brown", 32, "New York No. 1 Lake Park", 
+            [
+                new TagInfo { Name = "NICE", Color      = "green"},
+                new TagInfo { Name = "DEVELOPER", Color = "geekblue"}
+            ]),
+            new ("2", "Jim Green", 42, "London No. 1 Lake Park", 
+            [
+                new TagInfo { Name = "LOSER", Color = "volcano"}
+            ]),
+            new ("3", "Joe Black", 32, "Sydney No. 1 Lake Park", 
+            [
+                new TagInfo { Name = "COOL", Color    = "green"},
+                new TagInfo { Name = "TEACHER", Color = "geekblue"}
+            ])
         ];
         BasicCaseDataSource.AddRange(items);
+    }
+
+    private void InitFilterAndSorterDataSource()
+    {
+        List<DataGridBaseInfo> items = [
+            new ("1", "John Brown", 32, "New York No. 1 Lake Park", []),
+            new ("2", "Jim Green", 42, "London No. 1 Lake Park", []),
+            new ("3", "Joe Black", 32, "Sydney No. 1 Lake Park", []),
+            new ("4", "Joe Red", 32, "London No. 2 Lake Park", [])
+        ];
+        FilterAndSorterDataSource.AddRange(items);
     }
 }
 
@@ -53,11 +67,11 @@ public class DataGridBaseInfo
 
     public DataGridBaseInfo(string key, string name, int age, string address, List<TagInfo> tags)
     {
-        Key = key;
-        Name = name;
-        Age = age;
+        Key     = key;
+        Name    = name;
+        Age     = age;
         Address = address;
-        Tags = tags;
+        Tags    = tags;
     }
 }
 
