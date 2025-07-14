4d596a25501a16396ae7d0b22ad27fceba699791
chinboy <chinware@163.com>
2025-07-14 22:02:28 +0800

    Add DataGrid Column reorder

--- src/AtomUIGallery/ShowCases/Views/DataDisplay/DataGridShowCase.axaml.cs
+++ src/AtomUIGallery/ShowCases/Views/DataDisplay/DataGridShowCase.axaml.cs
@@ -15,46 +15,46 @@ public partial class DataGridShowCase : ReactiveUserControl<DataGridViewModel>
         {
             if (DataContext is DataGridViewModel viewModel)
             {
-                // BasicCaseGrid.ItemsSource                  = viewModel.BasicCaseDataSource;
-                // SelectionDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
-                // FilterAndSortGrid.ItemsSource              = viewModel.FilterAndSorterDataSource;
-                // FilterInTreeGrid.ItemsSource               = viewModel.FilterAndSorterDataSource;
-                // MultiSorterDataGrid.ItemsSource            = viewModel.MultiSorterDataSource;
-                // ResetFilterAndSortGrid.ItemsSource         = viewModel.BasicCaseDataSource;
-                // LargeSizeDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
-                // MiddleSizeDataGrid.ItemsSource             = viewModel.BasicCaseDataSource;
-                // SmallSizeDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
-                // CustomHeaderAndFooterDataGrid.ItemsSource  = viewModel.BasicCaseDataSource;
-                // ExpandableDataGrid.ItemsSource             = viewModel.ExpandableRowDataSource;
-                // OrderSpecificColumnDataGrid.ItemsSource    = viewModel.ExpandableRowDataSource;
-                // RowAndColumnHeaderDataGrid.ItemsSource     = viewModel.ExpandableRowDataSource;
-                // GroupHeaderDataGrid.ItemsSource            = viewModel.GroupHeaderDataSource;
-                // HideColumnDataGrid.ItemsSource             = viewModel.BasicCaseDataSource;
-                // FixedHeaderDataGrid.ItemsSource            = viewModel.FixedHeaderDataSource;
-                // FixedColumnsDataGrid.ItemsSource           = viewModel.FixedColumnsDataSource;
-                // FixedColumnsAndHeadersDataGrid.ItemsSource = viewModel.FixedColumnsAndHeadersDataSource;
+                BasicCaseGrid.ItemsSource                  = viewModel.BasicCaseDataSource;
+                SelectionDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
+                FilterAndSortGrid.ItemsSource              = viewModel.FilterAndSorterDataSource;
+                FilterInTreeGrid.ItemsSource               = viewModel.FilterAndSorterDataSource;
+                MultiSorterDataGrid.ItemsSource            = viewModel.MultiSorterDataSource;
+                ResetFilterAndSortGrid.ItemsSource         = viewModel.BasicCaseDataSource;
+                LargeSizeDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
+                MiddleSizeDataGrid.ItemsSource             = viewModel.BasicCaseDataSource;
+                SmallSizeDataGrid.ItemsSource              = viewModel.BasicCaseDataSource;
+                CustomHeaderAndFooterDataGrid.ItemsSource  = viewModel.BasicCaseDataSource;
+                ExpandableDataGrid.ItemsSource             = viewModel.ExpandableRowDataSource;
+                OrderSpecificColumnDataGrid.ItemsSource    = viewModel.ExpandableRowDataSource;
+                RowAndColumnHeaderDataGrid.ItemsSource     = viewModel.ExpandableRowDataSource;
+                GroupHeaderDataGrid.ItemsSource            = viewModel.GroupHeaderDataSource;
+                HideColumnDataGrid.ItemsSource             = viewModel.BasicCaseDataSource;
+                FixedHeaderDataGrid.ItemsSource            = viewModel.FixedHeaderDataSource;
+                FixedColumnsDataGrid.ItemsSource           = viewModel.FixedColumnsDataSource;
+                FixedColumnsAndHeadersDataGrid.ItemsSource = viewModel.FixedColumnsAndHeadersDataSource;
                 DragColumnDataGrid.ItemsSource             = viewModel.DragColumnDataSource;
             }
 
-            // ExtendedSelection.IsCheckedChanged += SelectionModeCheckedChanged;
-            // SingleSelection.IsCheckedChanged   += SelectionModeCheckedChanged;
-            //
-            // SortAgeBtn.Click                += HandleSortAgeBtnClick;
-            // ClearFiltersBtn.Click           += HandleClearFiltersBtnClick;
-            // ClearFiltersAndSortersBtn.Click += HandleClearFiltersAndSortersBtnClick;
-            // ColumnCheckBox1.IsChecked       =  true;
-            // ColumnCheckBox2.IsChecked       =  true;
-            // ColumnCheckBox3.IsChecked       =  true;
-            // ColumnCheckBox4.IsChecked       =  true;
-            // ColumnCheckBox5.IsChecked       =  true;
-            // ColumnCheckBox6.IsChecked       =  true;
-            //
-            // ColumnCheckBox1.IsCheckedChanged += HandleColumnVisibleChanged;
-            // ColumnCheckBox2.IsCheckedChanged += HandleColumnVisibleChanged;
-            // ColumnCheckBox3.IsCheckedChanged += HandleColumnVisibleChanged;
-            // ColumnCheckBox4.IsCheckedChanged += HandleColumnVisibleChanged;
-            // ColumnCheckBox5.IsCheckedChanged += HandleColumnVisibleChanged;
-            // ColumnCheckBox6.IsCheckedChanged += HandleColumnVisibleChanged;
+            ExtendedSelection.IsCheckedChanged += SelectionModeCheckedChanged;
+            SingleSelection.IsCheckedChanged   += SelectionModeCheckedChanged;
+            
+            SortAgeBtn.Click                += HandleSortAgeBtnClick;
+            ClearFiltersBtn.Click           += HandleClearFiltersBtnClick;
+            ClearFiltersAndSortersBtn.Click += HandleClearFiltersAndSortersBtnClick;
+            ColumnCheckBox1.IsChecked       =  true;
+            ColumnCheckBox2.IsChecked       =  true;
+            ColumnCheckBox3.IsChecked       =  true;
+            ColumnCheckBox4.IsChecked       =  true;
+            ColumnCheckBox5.IsChecked       =  true;
+            ColumnCheckBox6.IsChecked       =  true;
+            
+            ColumnCheckBox1.IsCheckedChanged += HandleColumnVisibleChanged;
+            ColumnCheckBox2.IsCheckedChanged += HandleColumnVisibleChanged;
+            ColumnCheckBox3.IsCheckedChanged += HandleColumnVisibleChanged;
+            ColumnCheckBox4.IsCheckedChanged += HandleColumnVisibleChanged;
+            ColumnCheckBox5.IsCheckedChanged += HandleColumnVisibleChanged;
+            ColumnCheckBox6.IsCheckedChanged += HandleColumnVisibleChanged;
         });
         InitializeComponent();
     }
@@ -63,72 +63,72 @@ public partial class DataGridShowCase : ReactiveUserControl<DataGridViewModel>
     {
         if (sender is RadioButton radioButton)
         {
-            // if (radioButton == ExtendedSelection && ExtendedSelection.IsChecked.HasValue &&
-            //     ExtendedSelection.IsChecked.Value)
-            // {
-            //     SelectionDataGrid.SelectionMode = DataGridSelectionMode.Extended;
-            // }
-            // else if (radioButton == SingleSelection && SingleSelection.IsChecked.HasValue &&
-            //          SingleSelection.IsChecked.Value)
-            // {
-            //     SelectionDataGrid.SelectionMode = DataGridSelectionMode.Single;
-            // }
+            if (radioButton == ExtendedSelection && ExtendedSelection.IsChecked.HasValue &&
+                ExtendedSelection.IsChecked.Value)
+            {
+                SelectionDataGrid.SelectionMode = DataGridSelectionMode.Extended;
+            }
+            else if (radioButton == SingleSelection && SingleSelection.IsChecked.HasValue &&
+                     SingleSelection.IsChecked.Value)
+            {
+                SelectionDataGrid.SelectionMode = DataGridSelectionMode.Single;
+            }
         }
     }
 
     private void HandleColumnVisibleChanged(object? sender, RoutedEventArgs e)
     {
-        // if (sender is CheckBox checkBox)
-        // {
-        //     var columns     = HideColumnDataGrid.Columns;
-        //     var name        = checkBox.Name;
-        //     var columnIndex = -1;
-        //     if (name == "ColumnCheckBox1")
-        //     {
-        //         columnIndex = 0;
-        //     }
-        //     else if (name == "ColumnCheckBox2")
-        //     {
-        //         columnIndex = 1;
-        //     }
-        //     else if (name == "ColumnCheckBox3")
-        //     {
-        //         columnIndex = 2;
-        //     }
-        //     else if (name == "ColumnCheckBox4")
-        //     {
-        //         columnIndex = 3;
-        //     }
-        //     else if (name == "ColumnCheckBox5")
-        //     {
-        //         columnIndex = 4;
-        //     }
-        //     else if (name == "ColumnCheckBox6")
-        //     {
-        //         columnIndex = 5;
-        //     }
-        //
-        //     if (columnIndex != -1)
-        //     {
-        //         var column = columns[columnIndex];
-        //         column.IsVisible = checkBox.IsChecked == true;
-        //     }
-        // }
+        if (sender is CheckBox checkBox)
+        {
+            var columns     = HideColumnDataGrid.Columns;
+            var name        = checkBox.Name;
+            var columnIndex = -1;
+            if (name == "ColumnCheckBox1")
+            {
+                columnIndex = 0;
+            }
+            else if (name == "ColumnCheckBox2")
+            {
+                columnIndex = 1;
+            }
+            else if (name == "ColumnCheckBox3")
+            {
+                columnIndex = 2;
+            }
+            else if (name == "ColumnCheckBox4")
+            {
+                columnIndex = 3;
+            }
+            else if (name == "ColumnCheckBox5")
+            {
+                columnIndex = 4;
+            }
+            else if (name == "ColumnCheckBox6")
+            {
+                columnIndex = 5;
+            }
+        
+            if (columnIndex != -1)
+            {
+                var column = columns[columnIndex];
+                column.IsVisible = checkBox.IsChecked == true;
+            }
+        }
     }
 
     private void HandleSortAgeBtnClick(object? sender, RoutedEventArgs? eventArgs)
     {
-        // ResetFilterAndSortGrid.Sort(1, ListSortDirection.Descending);
+        ResetFilterAndSortGrid.Sort(1, ListSortDirection.Descending);
     }
 
     private void HandleClearFiltersBtnClick(object? sender, RoutedEventArgs? eventArgs)
     {
-        // ResetFilterAndSortGrid.ClearFilters();
+        ResetFilterAndSortGrid.ClearFilters();
     }
 
     private void HandleClearFiltersAndSortersBtnClick(object? sender, RoutedEventArgs? eventArgs)
     {
-        // ResetFilterAndSortGrid.ClearFilters();
-        // ResetFilterAndSortGrid.ClearSort();
+        ResetFilterAndSortGrid.ClearFilters();
+        ResetFilterAndSortGrid.ClearSort();
     }
 }
