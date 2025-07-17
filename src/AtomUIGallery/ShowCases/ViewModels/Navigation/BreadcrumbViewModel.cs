4061350696a6d15f2cd334f33824d7694556b767
elarity <elarity@gmail.com>
2025-07-17 17:14:23 +0800

    Breadcrumb showcase milestone

--- src/AtomUIGallery/ShowCases/ViewModels/Navigation/BreadcrumbViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/Navigation/BreadcrumbViewModel.cs
@@ -1,31 +1,51 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
 using AtomUI.Controls;
-using Avalonia.Interactivity;
 using ReactiveUI;
-using AtomUI.Controls;
+using AtomUI.Controls.Utils;
+using AtomUI.IconPkg.AntDesign;
 
 namespace AtomUIGallery.ShowCases.ViewModels;
-
-/*
- * @desc: 面包屑中items数据结构
- */
-public class _BreadcrumbItem
-{
-    public string? Title   { get; set; }
-    public string? Separator   { get; set; }
-    public ICommand? Click { get; set; }   // 如需跳转
-}
 
 public class BreadcrumbViewModel : ReactiveObject, IRoutableViewModel
 {
     public const string ID = "BreadcrumbShowCase";
-
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItems { get; } = new()
+    
+    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase1 { get; } = new()
     {
-        new BreadcrumbItem() { Title = "Home", Separator               ="-" },
-        new BreadcrumbItem() { Title = "IOS", Separator ="-" },
-        new BreadcrumbItem() { Title = "APP", Separator   ="-" },
+        new BreadcrumbItem() { Title = "Home" },
+        new BreadcrumbItem() { Title = "Application Center" },
+        new BreadcrumbItem() { Title = "Application List" },
+        new BreadcrumbItem() { Title = "An Application" },
+    };
+    
+    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase2 { get; } = new()
+    {
+        new BreadcrumbItem() { Icon = new AntDesignIconPackage().BuildIcon("HomeOutlined") },
+        new BreadcrumbItem() { Title = "Application List", Icon = new AntDesignIconPackage().BuildIcon("UserOutlined") },
+        new BreadcrumbItem() { Title = "Application" },
+    };
+    
+    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase3 { get; } = new()
+    {
+        new BreadcrumbItem() { Icon = new AntDesignIconPackage().BuildIcon("HomeOutlined") },
+        new BreadcrumbItem() { Title = "Application List", Icon = new AntDesignIconPackage().BuildIcon("UserOutlined") },
+        new BreadcrumbItem() { Title = "Application", Value = "War3"},
+    };
+    
+    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase4 { get; } = new()
+    {
+        new BreadcrumbItem() { Title = "Home" },
+        new BreadcrumbItem() { Title = "Application Center" },
+        new BreadcrumbItem() { Title = "Application List" },
+        new BreadcrumbItem() { Title = "An Application" },
+    };
+    
+    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase5 { get; } = new()
+    {
+        new BreadcrumbItem() { Title = "Location", Separator = ":"},
+        new BreadcrumbItem() { Title = "Application Center" },
+        new BreadcrumbItem() { Title = "Application List" },
+        new BreadcrumbItem() { Title = "An Application" },
     };
     
     public IScreen HostScreen { get; }
