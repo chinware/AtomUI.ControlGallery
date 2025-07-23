f16bec5502fc809275c979fe8a649de941da8104
elarity <elarity@gmail.com>
2025-07-23 16:07:58 +0800

    Breadcrumb re-make

--- src/AtomUIGallery/ShowCases/ViewModels/Navigation/BreadcrumbViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/Navigation/BreadcrumbViewModel.cs
@@ -10,44 +10,6 @@ public class BreadcrumbViewModel : ReactiveObject, IRoutableViewModel
 {
     public const string ID = "BreadcrumbShowCase";
     
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase1 { get; } = new()
-    {
-        new BreadcrumbItem() { Title = "Home" },
-        new BreadcrumbItem() { Title = "Application Center" },
-        new BreadcrumbItem() { Title = "Application List" },
-        new BreadcrumbItem() { Title = "An Application" },
-    };
-    
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase2 { get; } = new()
-    {
-        new BreadcrumbItem() { Icon = new AntDesignIconPackage().BuildIcon("HomeOutlined") },
-        new BreadcrumbItem() { Title = "Application List", Icon = new AntDesignIconPackage().BuildIcon("UserOutlined") },
-        new BreadcrumbItem() { Title = "Application" },
-    };
-    
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase3 { get; } = new()
-    {
-        new BreadcrumbItem() { Icon = new AntDesignIconPackage().BuildIcon("HomeOutlined") },
-        new BreadcrumbItem() { Title = "Application List", Icon = new AntDesignIconPackage().BuildIcon("UserOutlined") },
-        new BreadcrumbItem() { Title = "Application", Value = "War3"},
-    };
-    
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase4 { get; } = new()
-    {
-        new BreadcrumbItem() { Title = "Home" },
-        new BreadcrumbItem() { Title = "Application Center" },
-        new BreadcrumbItem() { Title = "Application List" },
-        new BreadcrumbItem() { Title = "An Application" },
-    };
-    
-    public ObservableCollection<BreadcrumbItem> BreadcrumbItemsCase5 { get; } = new()
-    {
-        new BreadcrumbItem() { Title = "Location", Separator = ":"},
-        new BreadcrumbItem() { Title = "Application Center" },
-        new BreadcrumbItem() { Title = "Application List" },
-        new BreadcrumbItem() { Title = "An Application" },
-    };
-    
     public IScreen HostScreen { get; }
     
     public string UrlPathSegment { get; } = ID;
