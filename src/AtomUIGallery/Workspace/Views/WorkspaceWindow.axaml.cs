831947ccc4ea104522185a9b9a8fb2a23705476e
qinware <qinware@163.com>
2025-08-04 16:30:24 +0800

    Add IsCheckStateChanged for MenuItem

--- src/AtomUIGallery/Workspace/Views/WorkspaceWindow.axaml.cs
+++ src/AtomUIGallery/Workspace/Views/WorkspaceWindow.axaml.cs
@@ -1,8 +1,22 @@
-﻿using AtomUI.ReactiveUI;
+﻿using AtomUI.Controls;
+using AtomUI.ReactiveUI;
 using AtomUIGallery.Workspace.ViewModes;
 using Avalonia;
+using Avalonia.Interactivity;
 
 namespace AtomUIGallery.Workspace.Views;
+
+internal enum WindowMenuItemKind 
+{
+    FullScreen,
+    Pin,
+    Minimize,
+    Maximize,
+    Move,
+    Close,
+    Resize,
+    DarkMode
+}
 
 public partial class WorkspaceWindow : ReactiveWindow<WorkspaceWindowViewModel>
 {
@@ -13,12 +27,44 @@ public partial class WorkspaceWindow : ReactiveWindow<WorkspaceWindowViewModel>
 #endif
         DataContext = new WorkspaceWindowViewModel();
         InitializeComponent();
+        AddHandler(MenuItem.IsCheckStateChangedEvent, HandleMenuItemCheckChanged);
     }
 
     public override void Show()
     {
         base.Show();
-        Height    = double.NaN;
-        Width     = double.NaN;
+        Height = double.NaN;
+        Width  = double.NaN;
+    }
+
+    private void HandleMenuItemCheckChanged(object? sender, RoutedEventArgs e)
+    {
+        if (e.Source is MenuItem menuItem && menuItem.Tag is WindowMenuItemKind kind)
+        {
+            if (kind == WindowMenuItemKind.FullScreen)
+            {
+                IsFullScreenCaptionButtonEnabled = menuItem.IsChecked;
+            }
+            else if (kind == WindowMenuItemKind.Pin)
+            {
+                IsPinCaptionButtonEnabled = menuItem.IsChecked;
+            }
+            else if (kind == WindowMenuItemKind.Minimize)
+            {
+                IsMinimizeCaptionButtonEnabled = menuItem.IsChecked;
+            }
+            else if (kind == WindowMenuItemKind.Maximize)
+            {
+                IsMaximizeCaptionButtonEnabled = menuItem.IsChecked;
+            }
+            else if (kind == WindowMenuItemKind.Move)
+            {
+                IsMoveEnabled = menuItem.IsChecked;
+            }
+            else if (kind == WindowMenuItemKind.Resize)
+            {
+                IsResizeEnabled = menuItem.IsChecked;
+            }
+        }
     }
 }
