3e8fbc3215b34f5d8827b3033120141b77f990fa
qinware <qinware@163.com>
2025-08-08 11:29:12 +0800

    Refactor dark mode for AboutUsShowCase

--- src/AtomUIGallery/ShowCases/Views/General/AboutUsShowCase.axaml.cs
+++ src/AtomUIGallery/ShowCases/Views/General/AboutUsShowCase.axaml.cs
@@ -1,16 +1,32 @@
+using AtomUI.Data;
 using AtomUIGallery.ShowCases.ViewModels;
+using Avalonia;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
+using Application = AtomUI.Application;
 
 namespace AtomUIGallery.ShowCases.Views;
 
 public partial class AboutUsShowCase : ReactiveUserControl<AboutUsViewModel>
 {
+    public static readonly StyledProperty<bool> IsDarkThemeModeProperty =
+        Application.IsDarkThemeModeProperty.AddOwner<AboutUsShowCase>();
+    
+    public bool IsDarkThemeMode
+    {
+        get => GetValue(IsDarkThemeModeProperty);
+        set => SetValue(IsDarkThemeModeProperty, value);
+    }
+    
     public AboutUsShowCase()
     {
         this.WhenActivated(disposables =>
         {
-           
+            var application = Application.GetInstance();
+            if (application != null)
+            {
+                disposables.Add(BindUtils.RelayBind(application, Application.IsDarkThemeModeProperty, this, IsDarkThemeModeProperty));
+            }
         });
         InitializeComponent();
     }
