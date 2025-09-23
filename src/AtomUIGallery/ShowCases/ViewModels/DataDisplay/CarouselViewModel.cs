1301912caa0cb2d8cc6ce865dfeb5d9067f2567d
chinboy <qinware@163.com>
2025-09-23 23:32:19 +0800

    Implementing Carousel pagination position settings

--- src/AtomUIGallery/ShowCases/ViewModels/DataDisplay/CarouselViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/DataDisplay/CarouselViewModel.cs
@@ -1,3 +1,4 @@
+using AtomUI.Controls;
 using ReactiveUI;
 
 namespace AtomUIGallery.ShowCases.ViewModels;
@@ -9,6 +10,14 @@ public class CarouselViewModel : ReactiveObject, IRoutableViewModel
     public IScreen HostScreen { get; }
     
     public string UrlPathSegment { get; } = ID;
+    
+    private CarouselPaginationPosition _paginationPosition = CarouselPaginationPosition.Bottom;
+
+    public CarouselPaginationPosition PaginationPosition
+    {
+        get => _paginationPosition;
+        set => this.RaiseAndSetIfChanged(ref _paginationPosition, value);
+    }
     
     public CarouselViewModel(IScreen screen)
     {
--- src/AtomUIGallery/ShowCases/Views/DataDisplay/CarouselShowCase.axaml.cs
+++ src/AtomUIGallery/ShowCases/Views/DataDisplay/CarouselShowCase.axaml.cs
@@ -1,5 +1,8 @@
+using System.Reactive.Disposables;
+using AtomUI.Controls;
 using AtomUIGallery.ShowCases.ViewModels;
 using Avalonia.ReactiveUI;
+using ReactiveUI;
 
 namespace AtomUIGallery.ShowCases.Views;
 
@@ -7,6 +10,35 @@ public partial class CarouselShowCase : ReactiveUserControl<CarouselViewModel>
 {
     public CarouselShowCase()
     {
+        this.WhenActivated(disposables =>
+        {
+            PositionOptionGroup.OptionCheckedChanged += HandlePositionOptionChanged;
+            disposables.Add(Disposable.Create(() => PositionOptionGroup.OptionCheckedChanged -= HandlePositionOptionChanged));
+        });
         InitializeComponent();
+    }
+    
+    public void HandlePositionOptionChanged(object? sender, OptionCheckedChangedEventArgs args)
+    {
+        if (DataContext is CarouselViewModel viewModel)
+        {
+            if (args.Index == 0)
+            {
+                viewModel.PaginationPosition = CarouselPaginationPosition.Top;
+            }
+            else if (args.Index == 1)
+            {
+                viewModel.PaginationPosition = CarouselPaginationPosition.Bottom;
+            }
+            else if (args.Index == 2)
+            {
+                viewModel.PaginationPosition = CarouselPaginationPosition.Left;
+            }
+            else
+            {
+                viewModel.PaginationPosition = CarouselPaginationPosition.Right;
+            }   
+        }
+     
     }
 }
