c3cbb4f569e8eda69621adc2dfbd53f8419de4e7
chinboy <qinware@163.com>
2025-10-08 13:07:28 +0800

    Completion dot step indicator

--- src/AtomUIGallery/ShowCases/ViewModels/Navigation/StepsViewModel.cs
+++ src/AtomUIGallery/ShowCases/ViewModels/Navigation/StepsViewModel.cs
@@ -10,6 +10,26 @@ public class StepsViewModel : ReactiveObject, IRoutableViewModel
 
     public string UrlPathSegment { get; } = ID;
     
+    private int _currentStep;
+
+    public int CurrentStep
+    {
+        get => _currentStep;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _currentStep, value);
+            PreviousButtonVisible = CurrentStep > 0;
+        }
+    }
+
+    private bool _previousButtonVisible;
+
+    public bool PreviousButtonVisible
+    {
+        get => _previousButtonVisible;
+        set => this.RaiseAndSetIfChanged(ref _previousButtonVisible, value);
+    }
+    
     public StepsViewModel(IScreen screen)
     {
         HostScreen = screen;
